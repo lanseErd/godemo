dd4851c4667c67d82dbb5e255a2892d3b3ccae4d
lanse <619592413@qq.com>
2022-09-26 21:33:55 +0800

    qq

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("login", func(context *gin.Context) {
+		context.AsciiJSON(200, gin.H{
+			"statusCode": 200,
+			"data":       "hello",
+		})
+	})
+	log.Fatal(router.Run(":9006"))
+}
